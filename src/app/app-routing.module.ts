627ad9e251efb88b5d6bbc6df902fd460fbbfde1
Thyago Lucas <thyago.devin@gmail.com>
2022-12-15 16:19:51 -0400

    routers implementeds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardsComponent } from './views/dashboards/dashboards.component';
+import { EditMyDatasComponent } from './views/edit-my-datas/edit-my-datas.component';
+import { HomeComponent } from './views/home/home.component';
+import { PropertyComponent } from './views/property/property.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",
+  component: HomeComponent
+},{
+  path:"editar-imoveis",
+  component: PropertyComponent
+},{
+  path:"dashboard",
+  component: DashboardsComponent
+},{
+  path:"editar-dados",
+  component: EditMyDatasComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
