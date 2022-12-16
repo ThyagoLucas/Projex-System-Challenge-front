c891121c8201a28e4f984d3fb449cc3a21f15189
Thyago Lucas <56925523+ThyagoLucas@users.noreply.github.com>
2022-12-16 16:55:45 -0400

    Revert "Feat navigation"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,40 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './views/home/home.component';
-import { MatCardModule } from '@angular/material/card';
-import { EditMyDatasComponent } from './views/edit-my-datas/edit-my-datas.component';
-import { DashboardsComponent } from './views/dashboards/dashboards.component';
-import { LoginComponent } from './views/login/login.component';
-
+import { FooterComponent } from './components/template/footer/footer.component'
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent,
-    HomeComponent,
-    EditMyDatasComponent,
-    DashboardsComponent,
-    LoginComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatCardModule,
-
-    
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
